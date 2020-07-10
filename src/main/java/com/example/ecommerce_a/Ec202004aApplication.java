eea6d4ada1a55b51319bcf10770dbb3041fbde97
HyogaIto <hyoga.ito@rakus.co.jp>
2020-07-10 12:01:01 +0900

    first commit

---
+++ src/main/java/com/example/ecommerce_a/Ec202004aApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec202004aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec202004aApplication.class, args);
+	}
+
+}
