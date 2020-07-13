ef12403955f168b015aa95523a686dc35bb9cae2
HirotoKitamura <hiroto.kitamura@rakus.co.jp>
2020-07-13 12:27:50 +0900

    ユーザー登録画面で郵便番号から住所が自動入力される機能を実装

---
+++ src/main/resources/static/js/zipcode.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+$(function() {
+	$("#buttonZipcode").on("click", function() {
+		$.ajax({
+			url: "https://zipcoda.net/api",
+			dataType: "jsonp",
+			data: { 
+				zipcode: $("#inputZipcode").val().replace("-","")
+			},
+			async: true
+		}).done(function(data) {
+			console.dir(JSON.stringify(data));
+			$("#inputAddress").val(data.items[0].pref+data.items[0].address);
+		}).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+			// 検索失敗時には、その旨をダイアログ表示
+			alert("番号に該当する住所は存在しません。");
+			console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+			console.log("textStatus     : " + textStatus);
+			console.log("errorThrown    : " + errorThrown.message);
+		}).always(function(data) {
+		});
+	})
+});
