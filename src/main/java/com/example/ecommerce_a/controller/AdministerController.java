a59471c090795673f8b429ae70a5bdd2a85442cb
HyogaIto <hyoga.ito@rakus.co.jp>
2020-07-15 12:25:54 +0900

    administerController&Serviceのひな型を作成

---
+++ src/main/java/com/example/ecommerce_a/controller/AdministerController.java
@@ -0,0 +1,12 @@
+package com.example.ecommerce_a.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("administer")
+public class AdministerController {
+	
+	
+	
+}
---
+++ src/main/java/com/example/ecommerce_a/service/AdministerService.java
@@ -0,0 +1,9 @@
+package com.example.ecommerce_a.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdministerService {
+	
+	
+}
